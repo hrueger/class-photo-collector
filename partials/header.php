add134d3adddf21033cc662fbf99e6752e595b56
Hannes Rüger <hannesrueger@gmx.de>
2021-03-01 12:32:12 +0100

    fixed links

--- partials/header.php
+++ partials/header.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
       <div class="container">
-        <a href="/" class="navbar-brand">PHP Graph Tutorial</a>
+        <a href="/" class="navbar-brand">AG Klassenfotos 2021</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
             aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -8,11 +8,11 @@
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-              <a href="/" class="nav-link {{$_SERVER['REQUEST_URI'] == '/' ? ' active' : ''}}">Home</a>
+              <a href="index.php" class="nav-link <?php $_SERVER['REQUEST_URI'] == 'index.php' ? ' active' : '' ?>">Home</a>
             </li>
             <?php if (isLoggedin()) { ?>
               <li class="nav-item" data-turbolinks="false">
-                <a href="/calendar" class="nav-link <?php $_SERVER['REQUEST_URI'] == '/calendar' ? ' active' : ''?>">Calendar</a>
+                <a href="classes.php" class="nav-link <?php $_SERVER['REQUEST_URI'] == '/calendar' ? ' active' : ''?>">Calendar</a>
               </li>
             <?php } ?>
           </ul>
@@ -26,18 +26,18 @@
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
                   aria-haspopup="true" aria-expanded="false">
-                    <img src="profileImage.php" class="rounded-circle align-self-center mr-1" style="width: 32px;">
+                    <img src="profileImage.php" class="rounded-circle align-self-center mr-1" style="height: 25px;">
                 </a>
                 <div class="dropdown-menu dropdown-menu-right">
                   <h5 class="dropdown-item-text mb-0"><?php echo $_SESSION["username"] ?></h5>
                   <p class="dropdown-item-text text-muted mb-0"><?php echo $_SESSION["email"] ?></p>
                   <div class="dropdown-divider"></div>
-                  <a href="/signout" class="dropdown-item">Abmelden</a>
+                  <a href="signout.php" class="dropdown-item">Abmelden</a>
                 </div>
               </li>
             <?php } else { ?>
               <li class="nav-item">
-                <a href="/signin" class="nav-link">Sign In</a>
+                <a href="signin.php" class="nav-link">Sign In</a>
               </li>
             <?php } ?>
           </ul>
