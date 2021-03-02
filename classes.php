181af41f5c034ce6d83643bcac6f1bae329b6f11
Hannes Rüger <hannesrueger@gmx.de>
2021-03-02 09:57:22 +0100

    view user images in modal

--- classes.php
+++ classes.php
@@ -45,12 +45,12 @@ $jahrgangsstufen = array(
           <tr>
             <td><?php echo $user["username"]; ?></td>
             <td><?php echo $PHOTO_STATES_PRETTY[$user["photo_state"]]; ?></td>
-            <td><img class="d-block img-fluid userimg-small" src="serveImage.php?type=photo&userId=<?php echo $user["id"]; ?>"></td>
-            <td><img class="d-block img-fluid userimg-small" src="serveImage.php?type=privacy&userId=<?php echo $user["id"]; ?>"></td>
+            <td><img class="d-block img-fluid userimg-small cursor-pointer" onclick="openModal('<?php echo $user['id']; ?>', '<?php echo $user['username']; ?>', 'privacy')" src="serveImage.php?type=privacy&userId=<?php echo $user["id"]; ?>"></td>
+            <td><img class="d-block img-fluid userimg-small cursor-pointer" onclick="openModal('<?php echo $user['id']; ?>', '<?php echo $user['username']; ?>', 'photo')" src="serveImage.php?type=photo&userId=<?php echo $user["id"]; ?>"></td>
             <td>
               <a class="btn btn-outline-success"><i class="fas fa-check"></i></a>
-              <a class="btn btn-outline-danger"><i class="fas fa-times"></i></a>
-              <a class="btn btn-outline-danger"><i class="fas fa-times"></i></a>
+              <a class="btn btn-outline-danger"><i class="fas fa-user-times"></i></a>
+              <a class="btn btn-outline-danger"><i class="fas fa-file-alt"></i></a>
             </td>
           </tr>
         <?php
@@ -78,5 +78,34 @@ $jahrgangsstufen = array(
   <?php
   } ?>
 </div>
+
+<script>
+  function openModal(id, name, type) {
+    document.getElementById("modal-title").innerText = `${name} - ${type == "photo" ? "Foto" : "Datenschutzerklärung"}`;
+    document.getElementById("modal-image").src = `serveImage.php?type=${type}&userId=${id}`;
+    $("#imgModal").modal()
+
+  }
+</script>
+
+<div class="modal fade" tabindex="-1" id="imgModal">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modal-title">Loading...</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <img class="d-block img-fluid" id="modal-image" src="">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Schließen</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 <?php require_once("partials/foot.php"); ?>
--- partials/head.php
+++ partials/head.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Klassenfoto AG 2021</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/app.css">
 </head>
